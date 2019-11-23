99e43b120ec164a438e02395e769c70a09354269
J-Keven <jhonasnunes425@gmail.com>
2019-11-23 15:08:30 -0300

    Adicionado a função de busca, Deletar Arvore e Algoritimos de Percurso(Pre, In, e Pos-Ordem).

--- Arvore_Binaria/Arvore.c
+++ Arvore_Binaria/Arvore.c
@@ -15,19 +15,55 @@ No *inserir(No * arvore, int valor){
 		arvore = (No*)calloc(1,sizeof(No));
 		arvore->valor = valor;	
 	}
-	else if( valor > arvore->valor){
-		arvore->direita = inserir(arvore->direita,valor);
-	}
 	else if(valor < arvore->valor){
 		arvore->esquerda = inserir(arvore->esquerda, valor);
+	}
+	else{
+		arvore->direita = inserir(arvore->direita, valor);
 	}
 	return arvore;
 }
 
-void MostrarArvore(No * arvore){
+No * DestroiArvore(No * arvore){
 	if(arvore != NULL){
-		MostrarArvore(arvore->esquerda); 
-		printf("%d ", arvore->valor);
-		MostrarArvore(arvore->direita);
+		arvore->direita = DestroiArvore(arvore->direita);
+		arvore->esquerda = DestroiArvore(arvore->esquerda);
+		printf("Liberando no: %d", arvore->valor);
+		free(arvore);
 	}
+	return NULL;
+}
+
+int BuscaValor(No * arvore,int valor){
+	if(arvore == NULL){
+		return 0;
+	}
+	else if(arvore->valor == valor){
+		return 1;
+	}
+	return BuscaValor(arvore->esquerda, valor) || BuscaValor(arvore->direita, valor);
+}
+
+void MostrarPre_Ordem(No * arvore){
+	if(arvore != NULL){
+		printf("%d ", arvore->valor);
+		MostrarPre_Ordem(arvore->esquerda); 
+		MostrarPre_Ordem(arvore->direita);
+	}
+}
+
+void MostrarIn_Ordem(No * arvore){
+	if(arvore != NULL){
+		MostrarIn_Ordem(arvore->esquerda); 
+		printf("%d ", arvore->valor);
+		MostrarIn_Ordem(arvore->direita);
+	}
+}
+
+void MostrarPos_Ordem(No * arvore){
+	if(arvore != NULL){
+		MostrarPos_Ordem(arvore->esquerda); 
+		MostrarPos_Ordem(arvore->direita);
+		printf("%d ", arvore->valor);
+	}	
 }
