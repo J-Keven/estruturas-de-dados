13564539d911556b9ebd6a8f6465ec42782b44fc
J-Keven <jhonasnunes425@gmail.com>
2019-11-23 15:10:47 -0300

    Inserido novos casos de teste

--- Arvore_Binaria/main.c
+++ Arvore_Binaria/main.c
@@ -7,9 +7,14 @@ int main(int argc, char const *argv[])
 	arvore = inserir(arvore, 10);
 	arvore = inserir(arvore, 11);
 	arvore = inserir(arvore, 9);
+	arvore = inserir(arvore, 9);
 	arvore = inserir(arvore, 8);
 	arvore = inserir(arvore, 26);
+	arvore = inserir(arvore, 26);
 	arvore = inserir(arvore, 22);
-	MostrarArvore(arvore);
+	MostrarIn_Ordem(arvore);
+	printf("Esta na Arvore: %s\n", BuscaValor(arvore,26) ? "sim": "Nao");
+	printf("\n");
+	arvore = DestroiArvore(arvore);
 	return 0;
 }
