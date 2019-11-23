f77048636e33ee69a9768bb3960c2a6b2ffc1e8d
J-Keven <jhonasnunes425@gmail.com>
2019-11-23 15:09:59 -0300

    Adcionado os Escopos das funções no .h

--- Arvore_Binaria/arvore.h
+++ Arvore_Binaria/arvore.h
@@ -1,9 +1,12 @@
 #ifndef ARVORE_H
 #define ARVORE_H
 typedef struct no No;
-//Funções
 No *create();
 No *inserir(No * arvore, int valor);
 No *NoParaInserir(No * arvore, int valor);
-void MostrarArvore(No * arvore);
+No *DestroiArvore(No * arvore);
+int BuscaValor(No * arvore, int valor);
+void MostrarPre_Ordem(No * arvore);
+void MostrarIn_Ordem(No * arvore);
+void MostrarPos_Ordem(No * arvore);
 #endif
