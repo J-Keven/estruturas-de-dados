7461ad651d64fc75ce97d48289c789fff55058ac
J-keven <jhonasnunes425@gmail.com>
2019-12-01 23:58:43 -0300

    add função de inserir

--- Fila/FIla.c
+++ Fila/FIla.c
@@ -1,2 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "FIla.h"
+
+struct fila{
+    int value;
+    struct fila * next;
+    struct fila * andRow
+};
+
+Fila * InsertInRow(Fila * row, int value){
+    if(row == NULL){
+        return NULL;
+    }
+    Fila *newRow = (Fila *)calloc(1, sizeof(Fila));
+    newRow->value = value;
+    row->andRow->next = newRow;
+    row->andRow = newRow;
+    return row;
+}
