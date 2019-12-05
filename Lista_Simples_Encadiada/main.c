5cbac594e9f2300c754e328516e5a33606334ce9
J-keven <jhonasnunes425@gmail.com>
2019-12-04 22:33:53 -0300

    Add testes

--- Lista_Simples_Encadiada/main.c
+++ Lista_Simples_Encadiada/main.c
@@ -1,1 +1,25 @@
 #include "lista.h"
+int main(int argc, char const *argv[])
+{
+    No * list = create();
+    list = insert(list,1);
+    list = insert_In_Order(list,2);
+    list = insert_In_Order(list,3);
+    list = insert_In_Order(list,4);
+    list = insert_In_Order(list,5);
+    Show(list);
+    printf("\n");
+    list = Delete(list, 2);
+    Show(list);
+    printf("\n");
+    list = Delete(list, 5);
+    Show(list);
+    printf("\n");
+    list = Delete(list, 3);
+    Show(list);
+    printf("\n");
+    list = FreeList(list);
+    Show(list);
+    printf("\n");
+    return 0;
+}
