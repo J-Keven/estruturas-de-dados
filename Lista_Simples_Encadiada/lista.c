2f2ac674cf8c9c726efaf472ff4554d840b4f928
J-keven <jhonasnunes425@gmail.com>
2019-12-04 22:32:41 -0300

    Add mais funções

--- Lista_Simples_Encadiada/lista.c
+++ Lista_Simples_Encadiada/lista.c
@@ -26,18 +26,48 @@ No * insert_In_Order(No * list, int value){
     if(list == NULL){
         return new;
     }
+    else if(value < list->value){
+        new->next = list;
+        return new;
+    }
     No * aux_list = list;
-    for(;aux_list->next != NULL && value > aux_list->next->value; aux_list = aux_list->next);
+    for(;aux_list->next != NULL && value > aux_list->next->value;aux_list = aux_list->next);
     new->next = aux_list->next;
     aux_list->next = new;
+    return list;
 }
-// 0 1 2 4 5 6
-No * Delete(No * list, int value){
 
+No * Delete(No * list, int value){
+    if(list == NULL){
+        return NULL;
+    }
+    No * aux_list = list;
+    if(list->value == value){
+        aux_list = aux_list->next;
+        free(list);
+        return aux_list;
+    }
+    No * aux2 = aux_list;
+    do{
+        aux_list = aux_list->next;
+        if(aux_list != NULL && aux_list->value == value){
+            aux2->next = aux_list->next;
+            free(aux_list);
+            break;
+        }
+        aux2 = aux_list;
+    }while(aux2 != NULL);
+    return list;
 }
 
 No * FreeList(No * list){
-
+    No * aux;
+    while(list != NULL){
+        aux = list->next;
+        free(list);
+        list = aux;
+    }
+    return NULL;
 }
 
 int Search(No * list, int value){
