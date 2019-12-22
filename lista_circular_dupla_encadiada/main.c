f830b32f8b085fc5a9fa619ea0e865202f4fc6b0
J-keven <jhonasnunes425@gmail.com>
2019-12-22 14:06:18 -0300

    adding the menu

--- lista_circular_dupla_encadiada/main.c
+++ lista_circular_dupla_encadiada/main.c
@@ -15,3 +15,16 @@ int main(int argc, char const *argv[])
     list_show(list);
     return 0;
 }
+
+void Menu(){
+    printf("\nMENU\n");
+    printf("1 - inserir um valor\n");
+    printf("2 - verificar a existencia de um valor\n");
+    printf("3 - verficar o tamanho da lista\n");
+    printf("4 - exibir o primeiro valor da lista\n");
+    printf("5 - exibir toda a lista\n");
+    printf("6 - remover um valor\n");
+    printf("7 - liberar a lista\n");
+    printf("0 - sair\n");
+
+}
